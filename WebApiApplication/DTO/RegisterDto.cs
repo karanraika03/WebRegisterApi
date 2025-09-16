b7f28613675ec4280e4070a301cee19680566628
DELL <DELL@DESKTOP-6SGTUTO>
2025-09-16 20:57:31 +0530

    runing error solve

--- WebApiApplication/DTO/RegisterDto.cs
+++ WebApiApplication/DTO/RegisterDto.cs
@@ -5,5 +5,6 @@
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? PasswordHash { get; set; }
+        public int RoleId { get; set; }
     }
 }
